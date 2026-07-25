ddf2ea8b60466511fc47a9bc42c8153d49990a7e
Ahmed Galal <madogal95@gmail.com>
2026-07-26 01:16:21 +0300

    add my name

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,9 @@ import DefaultLayout from "@/layout/DefaultLayout";
 function App() {
   return (
     <>
-      <div className="min-h-screen transition-all duration-300 flex items-center justify-center p-6">
+      <div className="min-h-screen transition-all duration-300 flex flex-col items-center justify-center p-6">
         <DefaultLayout />
+        <span className="mt-5 text-sm text-black">Desined by: <a href="#" className="text-[18px] text-gray-600 hover:text-gray-500">Ahmed Galal</a></span>
       </div>
       <Toaster />
     </>
